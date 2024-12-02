80d95af71d64dc649eba621b44f894340831b4af
SaravanakumarThavamani <158137353+SaravanakumarThavamani@users.noreply.github.com>
2024-12-02 12:44:01 +0530

    Add files via upload

---
+++ Project1/lib/view/houses.dart
@@ -0,0 +1,101 @@
+import '../Controller/house.dart';
+import '../Controller/place.dart';
+
+final placeList = [
+  PlaceModel(
+      imageUrl:
+          'https://www.tripsavvy.com/thmb/Vm-xdyjrWpZiQgtV-rVbd7EsUBk=/2125x1416/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-580758423-56c6ab0c3df78cfb37860807.jpg',
+      title: 'India'),
+  PlaceModel(
+      imageUrl:
+          'https://pixelz.cc/wp-content/uploads/2018/07/red-square-at-night-moscow-russia-uhd-4k-walllpaper.jpg',
+      title: 'Moscow'),
+  PlaceModel(
+      imageUrl:
+          'https://a.cdn-hotels.com/gdcs/production79/d567/927124e0-6bc6-4123-b007-e2b235afab1a.jpg',
+      title: 'USA'),
+  PlaceModel(
+      imageUrl:
+          'https://th.bing.com/th/id/OIP.vfZTCuvE7jiiDqmRkviWLwHaE7?w=360&h=197&c=7&r=0&o=5&pid=1.7',
+      title: 'China'),
+  PlaceModel(
+      imageUrl:
+          'https://hips.hearstapps.com/bpc.h-cdn.co/assets/17/43/1600x800/landscape-1508872179-japan-tours.jpg?resize=980:*',
+      title: 'Japan'),
+];
+
+final houseList = [
+  HouseModel(
+      imageUrl:
+          'https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 4,
+      amount: 120,
+      availableBeds: 4),
+  HouseModel(
+      imageUrl:
+          'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Carinthia lake see Breakfast',
+      rating: 3,
+      amount: 90,
+      availableBeds: 5),
+  HouseModel(
+      imageUrl:
+          'https://images.pexels.com/photos/208736/pexels-photo-208736.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 5,
+      amount: 220,
+      availableBeds: 3),
+  HouseModel(
+      imageUrl:
+          'https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 3.6,
+      amount: 180,
+      availableBeds: 6),
+  HouseModel(
+      imageUrl:
+          'https://images.pexels.com/photos/2091166/pexels-photo-2091166.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Carinthia lake see Breakfast',
+      rating: 4.5,
+      amount: 300,
+      availableBeds: 4),
+  HouseModel(
+      imageUrl:
+          'https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 3.2,
+      amount: 140,
+      availableBeds: 3),
+];
+
+final mostViewedHouses = [
+  HouseModel(
+      imageUrl:
+          'https://static.vecteezy.com/system/resources/thumbnails/022/903/424/small/ai-generative-3d-modern-luxury-real-estate-house-for-sale-and-rent-luxury-property-concept-ai-generated-artwork-photo.jpg',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 4.6,
+      amount: 270,
+      availableBeds: 8),
+  HouseModel(
+      imageUrl:
+          'https://t3.ftcdn.net/jpg/01/33/61/48/360_F_133614856_wrBbpoXHLxseiVDBzT2paBVvjOE6WZcJ.jpg',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 4,
+      amount: 230,
+      availableBeds: 5),
+  HouseModel(
+      imageUrl:
+          'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEivM0EZrpZLV1ir1EBXXV1Df1wj-y4U0ft-Df_Vuvx1WCIMtBfRHQfsKQlew-eKFjJvw6SdwXjLA9rThaL8a9mYpT0xhMtOPjoJ5biwkRLqVHytoIf83Ur-BZlgkEix90300GXQh7L3vEVJ6xBuknynwnWtpxXwlCeCQuBlS7joGhtgRGTm4O0YOm_a/s16000/latest-house-design-trends.jpg',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 5,
+      amount: 300,
+      availableBeds: 4),
+  HouseModel(
+      imageUrl:
+          'https://media.istockphoto.com/id/1150545984/photo/upscale-modern-mansion-with-pool.jpg?s=612x612&w=0&k=20&c=JT7qSGgmlGfiNiqJE2jw6rYwRcYCj9KBs7i2Rmyyypo=',
+      title: 'Carinthia lake see Breakfast lorem',
+      rating: 4.8,
+      amount: 200,
+      availableBeds: 6),
+];
